040dc77f1fdde24168e5325585714235ed5aa8b3
claregillis <clare.gillis@mail.utoronto.ca>
2024-10-28 01:10:37 +0000

    initialized package structure and predicted necessary R files

---
+++ R/constants.R
@@ -0,0 +1,24 @@
+# Index ranges for CDRs in each renumbering scheme
+CHOTHIA_RANGES <- list(L1 = c(26, 32), L2 = c(50, 52), L3 = c(91, 96), 
+                       H1 = c(26, 32), H2 = c(52, 56), H3 = c(96, 101))
+KABAT_RANGES <- list(L1 = c(24, 34), L2 = c(50, 56), L3 = c(89, 97), 
+                     H1 = c(31, 35), H2 = c(50, 65), H3 = c(95, 102))
+AHO_RANGES <- list(L1 = c(24, 34), L2 = c(50, 56), L3 = c(89, 97), 
+                    H1 = c(31, 35), H2 = c(50, 65), H3 = c(95, 102))
+MARTIN_RANGES <- list(L1 = c(24, 34), L2 = c(50, 56), L3 = c(89, 97), 
+                      H1 = c(31, 35), H2 = c(50, 65), H3 = c(95, 102))
+IMGT_RANGES <- list(L1 = c(27, 32), L2 = c(50, 51), L3 = c(89, 97), 
+                    H1 = c(26, 35), H2 = c(51, 56), H3 = c(93, 102))
+
+default_colours = list(
+  H1 = '#44AA99',
+  H2 = '#DDCC77',
+  H3 = '#88CCEE',
+  L1 = '#44AA99',
+  L2 = '#DDCC77',
+  L3 = '#88CCEE',
+  heavy = '#AA4499',
+  light = "#117733",
+  antigen = '#882255',
+  other = '#332288'
+)
