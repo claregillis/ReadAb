26bd4423348d9a14e7c9605694ba86059deaffea
claregillis <clare.gillis@mail.utoronto.ca>
2024-11-27 15:14:10 -0500

    add function to run Shiny app

---
+++ R/runReadAb.R
@@ -0,0 +1,27 @@
+#' Launch Shiny App for ReadAb
+#'
+#' A function that launches the Shiny app for ReadAb
+#' The purpose of this app is only to illustrate how a Shiny
+#' app works. The code has been placed in \code{./inst/shiny-scripts}.
+#'
+#' @return No return value but open up a Shiny page.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' ReadAb::runReadAb()
+#' }
+#'
+#' @references
+#' Grolemund, G. (2015). Learn Shiny - Video Tutorials. \href{https://shiny.rstudio.com/tutorial/}{Link}
+#'
+#' @export
+#' @importFrom shiny runApp
+
+runReadAb <- function() {
+  appDir <- system.file("shiny-scripts",
+                        package = "ReadAb")
+  actionShiny <- shiny::runApp(appDir, display.mode = "normal")
+  return(actionShiny)
+}
+# [END]
